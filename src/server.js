33968ec9875da9837f9c496dd0294849ee42a37b
Khải Trần Quang <khaitran955@gmail.com>
2026-01-01 10:20:32 +0700

    [Init] Setup Project Base Structure

---
+++ src/server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+require("dotenv").config();
+
+app.get("/", (req, res) => {
+  res.send("Hello!");
+});
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
